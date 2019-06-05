e5cb6896072d8c72f2ea6f60cb1c7739d8700835
JayYin <yjf@mallcai.com>
2019-06-05 19:06:17 +0800

    GC & jvm memory aera not finish

---
+++ src/com/jayyin/spring/others/classloader/ClassLoaderTest.java
@@ -0,0 +1,29 @@
+package com.jayyin.spring.others.classloader;
+
+import java.net.URLClassLoader;
+
+/**
+ * @description This Class is for xxx use
+ * @author: jayyin
+ * @date: 2019-06-05 16:51
+ * @version: 1.0
+ */
+public class ClassLoaderTest {
+
+    public static void main(String[] v){
+
+//        ClassLoader
+
+        //系统类加载器
+        ClassLoader.getSystemClassLoader();
+
+
+        //从网络加载
+//        URLClassLoader
+
+    }
+
+
+
+
+}
